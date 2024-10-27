2bda7a6772d85101e03adbff8565c11838ea6f1b
Dwi Laras Setyadita <dwilaras.2022@student.uny.ac.id>
2024-10-27 12:42:59 +0700

    edit emotion images

--- dashboard.py
+++ dashboard.py
@@ -1,3 +1,5 @@
+
+
 import streamlit as st
 
 #######################
@@ -13,9 +15,74 @@ with open('./css/style.css') as f :
 # main method #
 ###############
 def main():
-    st.markdown('<h1 class="page-title">🌊 Jellyfish Classification Dashboard 🪼</h1>', unsafe_allow_html=True)
-    st.write('hello world')
+    st.markdown('<h1 class="page-title">Emotion Classification Dashboard</h1>', unsafe_allow_html=True)
+
     st.markdown('<hr class="custom-divider">', unsafe_allow_html=True)
+    st.markdown(
+        """
+        <div class="card">
+            <h3>Tentang</h3>
+            <p>Dashboard ini dibuat untuk membarikan pengetahuan dan wawasan tentang emosi manusia melalui analisis 
+            gambar. Dengan memanfaatkan teknologi machine learning dalam pengolahan citra gembar, kami akan membantu Anda
+            mengidentifikasi secara akurat, memahami dengan mendalam, dan memberikan saran atas emosi yang anda alami.</p>
+            
+        </div>
+        """,     unsafe_allow_html=True
+    )
+    # happy
+    col1, col2 = st.columns([1, 4])
+
+    with col1:
+        st.image('./images/happy.jpg', caption='Deskripsi Gambar')  # Menampilkan gambar
+
+    with col2:
+        st.write('hello')
+
+    # sad
+    col1, col2 = st.columns([1, 4])
+    with col1:
+
+        st.image('./images/sad.jpg', caption='Deskripsi Gambar')  # Menampilkan gambar
+
+    with col2:
+        st.write('hello')
+
+    # angry
+    col1, col2 = st.columns([1, 4])
+    with col1:
+
+        st.image('./images/neutral.jpg', caption='Deskripsi Gambar')  # Menampilkan gambar
+
+    with col2:
+        st.write('hello')
+
+    # surprised
+    col1, col2 = st.columns([1, 4])
+    with col1:
+
+        st.image('./images/surprised.jpg', caption='Deskripsi Gambar')  # Menampilkan gambar
+
+    with col2:
+        st.write('hello')
+
+    # neutral
+    col1, col2 = st.columns([1, 4])
+    with col1:
+
+        st.image('./images/neutral.jpg', caption='Deskripsi Gambar')  # Menampilkan gambar
+
+    with col2:
+        st.write('hello')
+
+    # ahegao
+    col1, col2 = st.columns([1, 4])
+    with col1:
+
+        st.image('./images/ahegao.jpg', caption='Deskripsi Gambar')  # Menampilkan gambar
+
+    with col2:
+        st.write('hello')
+
 
 #########################
 # menjalankan kode main #
