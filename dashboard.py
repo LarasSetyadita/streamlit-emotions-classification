25f62e665744ec93855296e30bd6cce73dddf309
Dwi Laras Setyadita <dwilaras.2022@student.uny.ac.id>
2024-10-27 10:27:55 +0700

    add dashboard.py

---
+++ dashboard.py
@@ -0,0 +1,24 @@
+import streamlit as st
+
+#######################
+# pengaturan tampilan #
+#######################
+st.set_page_config(layout='wide')
+
+# load css file
+with open('./css/style.css') as f :
+    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+###############
+# main method #
+###############
+def main():
+    st.markdown('<h1 class="page-title">🌊 Jellyfish Classification Dashboard 🪼</h1>', unsafe_allow_html=True)
+    st.write('hello world')
+    st.markdown('<hr class="custom-divider">', unsafe_allow_html=True)
+
+#########################
+# menjalankan kode main #
+#########################
+if __name__ == '__main__':
+    main()
